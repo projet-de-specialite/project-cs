50fb382e9ab311fa7168055f9e3f22c2a37782b4
Mouad <mouad10wolf@gmail.com>
2023-05-12 03:27:57 +0200

    Re-constructing the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,51 @@ import {ChakraProvider, theme,} from "@chakra-ui/react"
 
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Base from "./views/Base";
+import Home from "./views/Home";
 import SignUp from "./views/SignUp";
 import Login from "./views/Login";
 import AuthService from "./services/auth";
-import { redirect } from "react-router-dom";
-  
+import { redirect,Routes, Route } from "react-router-dom";
+
 const Logout = ()=>{
     AuthService.logout();
     return redirect("/login");    
 };
-const router = createBrowserRouter([
+// const router = createBrowserRouter([
 
-    {
-        path: "/",
-        element: <Base />,
-    },
-    {
-        path: "/signup",
-        element: <SignUp />,
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/logout",
-        loader: Logout,
-        element: <Login />,
-    },
+//     {
+//         path: "/",
+//         element: <Home />,
+//     },
+//     {
+//         path: "/signup",
+//         element: <SignUp />,
+//     },
+//     {
+//         path: "/login",
+//         element: <Login />,
+//     },
+//     {
+//         path: "/logout",
+//         loader: Logout,
+//         element: <Login />,
+//     },
 
-]);
+// ]);
 
 export const App = () => (
     <ChakraProvider theme={theme}>
-        <RouterProvider router={router}/>
+        <Routes>
+            
+            <Route path="/" element={<Base />}>
+                <Route index element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+            </Route>
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            
+        </Routes>
+        
+        {/* <RouterProvider router={router}/> */}
     </ChakraProvider>
 )
--- src/views/Base.tsx
+++ src/views/Base.tsx
@@ -5,18 +5,22 @@ import {
     Heading,
     Menu,
     MenuItem,
-    Text, Avatar, CardHeader, Flex, Box, CardFooter, Button, Image, SimpleGrid, Center, CardBody
+    Text, Avatar, CardHeader, Flex, Box, Center
 } from "@chakra-ui/react";
 import {
     BiBell,
     BiHome,
-    BiPaperPlane, BiPlusCircle, BiSearch,
+    BiPlusCircle, BiSearch,
     BiUserCircle,
-    BsChat,
-    BsHeart,
+
 } from "react-icons/all";
 
-export default function Base() {
+import { Outlet } from "react-router-dom";
+
+import  { ReactNode } from 'react';
+
+
+const Base = () => {
 
     return (
         <>
@@ -58,85 +62,8 @@ export default function Base() {
                 </GridItem>
                 <GridItem pl='2' py='5' area={'main'}>
 
-                    <SimpleGrid columns={1} spacing={10}>
-                        <Center>
-                            <Card maxW='md'>
-                                <CardHeader>
-                                    <Flex>
-                                        <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                                            <Avatar name='ChatRoon' src='https://i.pravatar.cc/150?img=49' />
-                                            <Box>
-                                                <Heading size='sm'>ChatRoon</Heading>
-                                                <Text fontSize='xs'>20 mars 2023</Text>
-                                            </Box>
-                                        </Flex>
-                                    </Flex>
-                                </CardHeader>
-                                <Image
-                                    objectFit='cover'
-                                    src='https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA'
-                                    alt='ChatRoon'
-                                />
-                                <CardBody>
-                                    <Text fontSize='xs'>
-                                        Beautiful sky
-                                    </Text>
-                                </CardBody>
-                                <CardFooter
-                                    justify='flex-start'
-                                    flexWrap='wrap'
-                                    sx={{
-                                        '& > button': {
-                                            minW: '136px',
-                                        },
-                                    }}
-                                >
-                                    <Button flex='1' variant='ghost' leftIcon={<BsHeart />}>20</Button>
-                                    <Button flex='1' variant='ghost' leftIcon={<BsChat />}>4</Button>
-                                    <Button flex='1' variant='ghost' leftIcon={<BiPaperPlane />}></Button>
-                                </CardFooter>
-                            </Card>
-                        </Center>
-                        <Center>
-                            <Card maxW='md'>
-                                <CardHeader>
-                                    <Flex>
-                                        <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                                            <Avatar name='Gaeitan SceauSix' src='https://i.pravatar.cc/150?img=68' />
+                    <Outlet />
 
-                                            <Box>
-                                                <Heading size='sm'>Gaeitan SceauSix</Heading>
-                                                <Text fontSize='xs'>20 mars 2023</Text>
-                                            </Box>
-                                        </Flex>
-                                    </Flex>
-                                </CardHeader>
-                                <Image
-                                    objectFit='cover'
-                                    src='https://fastly.picsum.photos/id/13/2500/1667.jpg?hmac=SoX9UoHhN8HyklRA4A3vcCWJMVtiBXUg0W4ljWTor7s'
-                                    alt='Gaeitan SceauSix'
-                                />
-                                <CardBody>
-                                    <Text fontSize='xs'>
-                                        Beautiful sky
-                                    </Text>
-                                </CardBody>
-                                <CardFooter
-                                    justify='flex-start'
-                                    flexWrap='wrap'
-                                    sx={{
-                                        '& > button': {
-                                            minW: '136px',
-                                        },
-                                    }}
-                                >
-                                    <Button flex='1' variant='ghost' leftIcon={<BsHeart />}>20</Button>
-                                    <Button flex='1' variant='ghost' leftIcon={<BsChat />}>4</Button>
-                                    <Button flex='1' variant='ghost' leftIcon={<BiPaperPlane />}></Button>
-                                </CardFooter>
-                            </Card>
-                        </Center>
-                    </SimpleGrid>
 
                 </GridItem>
 
@@ -162,3 +89,5 @@ export default function Base() {
         </>
     );
 }
+
+export default Base;
